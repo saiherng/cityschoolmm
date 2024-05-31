49ae6a2bdf98595c1b643cfd0d2c1bb05f0105e9
Sai Herng <saiherng14@gmail.com>
2024-05-31 15:14:18 -0700

    commnit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-620xors-1f@(+7id2hrbiemds7*94l9&pxaaeyhbn^9mw(%*5y"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
