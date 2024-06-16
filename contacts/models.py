55fe109cea88e8d85dc17888b26ae73ad108455a
saiherng <saiherng14@gmail.com>
2024-06-15 23:57:33 -0700

    updated contacts models for wagtail template

--- contacts/models.py
+++ contacts/models.py
@@ -1,15 +1,29 @@
 from django.db import models
-
-from wagtail.models import Page
+from modelcluster.fields import ParentalKey
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
 
+# import MultiFieldPanel:
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
 
 class ContactsIndexPage(Page):
     
-    body = RichTextField(blank=True)
+    phone_number = models.CharField(max_length=250)
+    address = models.TextField(help_text="Address Field", blank=True)
+    email = models.EmailField(max_length= 250)
+    page_url = models.URLField(help_text="Social Media Page URL")
+
     
     content_panels = Page.content_panels + [
-        FieldPanel('body'),
+        MultiFieldPanel
+            [
+                FieldPanel("phone_number"),
+                FieldPanel("email"),
+                FieldPanel("address"),
+                FieldPanel("page_url")
+
+            ],
     ]
+
+
 
